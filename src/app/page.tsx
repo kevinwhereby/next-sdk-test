53eed4040414d0e67490e72603d61df46f1dbcc9
Kevin Hanna <kevin.hanna@whereby.com>
2024-10-18 15:17:23 +0100

    test knock handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import dynamic from "next/dynamic";
 import * as React from "react";
 const Room = dynamic(() => import("../components/room"), { ssr: false });
 
-
-const roomUrl = "https://team.whereby.com/kevin"
+const roomUrl = "<room url>"
 export default function Home() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
--- src/components/room.tsx
+++ src/components/room.tsx
@@ -2,34 +2,74 @@
 
 import * as React from "react";
 
-import { useRoomConnection, VideoGrid } from "@whereby.com/browser-sdk/react";
+import { useLocalMedia, useRoomConnection, VideoGrid } from "@whereby.com/browser-sdk/react";
 import { useRouter } from 'next/navigation'
-
 
 interface Props {
     roomUrl: string;
 }
 
 export default function Room({ roomUrl }: Props) {
-    const { actions } = useRoomConnection(roomUrl, { localMediaOptions: { audio: true, video: true } });
-    const { joinRoom, leaveRoom } = actions;
     const router = useRouter()
 
+    const { state: localMediaState, actions: localMediaActions } = useLocalMedia({
+        video: true,
+        audio: true,
+    });
+
+    const { state, actions } = useRoomConnection(roomUrl, {
+        localMedia: {
+            state: localMediaState,
+            actions: localMediaActions,
+        },
+    });
+    const { joinRoom, leaveRoom } = actions
+
+    const handleKnock = async () => {
+        actions.knock();
+    };
 
     React.useEffect(() => {
-        joinRoom();
-        return () => {
-            console.log("HERE")
-            leaveRoom()
-        };
-    }, [joinRoom, leaveRoom]);
+        console.log(state);
+    }, [state]);
 
-    return (
-        <div style={{width: "100%", height: "100%"}}>
-            <VideoGrid />
-            <button type="button" onClick={() => router.push('/other')} style={{ width: "5em", height: "2em" }}>
-                Other
-            </button>
-        </div>
-    );
+    React.useEffect(() => {
+          joinRoom();
+          return () => {
+              leaveRoom()
+          };
+      }, [joinRoom, leaveRoom]);
+
+    if (state.connectionStatus === "ready") {
+        return (
+            <div>
+                Loading...
+            </div>
+        );
+    }
+
+    if (state.connectionStatus === "room_locked") {
+        return (
+            <div>
+                <button onClick={() => handleKnock()}>Knock</button>
+            </div>
+        );
+    }
+
+    if (state.connectionStatus === "knocking") {
+        return (
+            <div>
+                Knocking
+            </div>
+        );
+    }
+
+      return (
+          <div style={{width: "100%", height: "100%"}}>
+              <VideoGrid />
+              <button type="button" onClick={() => router.push('/other')} style={{ width: "5em", height: "2em" }}>
+                  Other
+              </button>
+          </div>
+      );
 }
